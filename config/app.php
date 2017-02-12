4a1b1b89dc2be5ed2722dc2e158285e4a6c15b06
Chris Gmyr <cmgmyr@gmail.com>
2017-02-12 09:10:05 -0500

    update messenger views, remove laravelcollective/html

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Cmgmyr\Messenger\MessengerServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -196,9 +195,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
